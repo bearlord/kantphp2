7524685147b40effde56911719c46ac001531a74
zzq <zhangsan6947008@126.com>
2017-02-26 17:47:24 +0800

    RuntimeException

---
+++ vendor/kantphp/Exception/RuntimeException.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @package KantPHP
+ * @author  Zhenqiang Zhang <565364226@qq.com>
+ * @copyright (c) 2011 KantPHP Studio, All rights reserved.
+ * @license http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ */
+
+namespace Kant\Exception;
+
+use Kant\Exception\KantException;
+use Kant\Http\Response;
+
+/**
+ * HttpException represents an exception caused by an improper request of the end-user.
+ *
+ * HttpException can be differentiated via its [[statusCode]] property value which
+ * keeps a standard HTTP status code (e.g. 404, 500). Error handlers may use this status code
+ * to decide how to format the error page.
+ *
+ * Throwing an HttpException like in the following example will result in the 404 page to be displayed.
+ *
+ * ```php
+ * if ($item === null) { // item does not exist
+ *     throw new \Kant\Exception\HttpException(404, 'The requested Item could not be found.');
+ * }
+ * ```
+ *
+ */
+class RumtimeException extends KantException {
+
+    /**
+     * @return string the user-friendly name of this exception
+     */
+    public function getName() {
+        return 'Runtime Exception';
+    }
+
+}
