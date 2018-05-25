d710a69a8c834897fd1a9bac979a0e27d1095c7b
zzq <565364226@qq.com>
2018-05-25 10:56:01 +0800

    ErrorHandler

---
+++ vendor/kantphp/Exception/UserException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kant\Exception;
+
+
+class UserException
+{
+
+}
---
+++ vendor/kantphp/Foundation/Exception.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package KantPHP
+ * @author  Zhenqiang Zhang <zhenqiang.zhang@hotmail.com>
+ * @copyright (c) KantPHP Studio, All rights reserved.
+ * @license http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ */
+
+namespace Kant\Foundation;
+
+/**
+ * Exception represents a generic exception for all purposes.
+ *
+ * For more details and usage information on Exception, see the [guide article on handling errors](guide:runtime-handling-errors).
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class Exception extends \Exception
+{
+    /**
+     * @return string the user-friendly name of this exception
+     */
+    public function getName()
+    {
+        return 'Exception';
+    }
+}
